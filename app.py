9d500f5ff8d19442f4ee5ec3e6b627ff0fec76f4
namanvarshney76-tech <namanvarshney76@gmail.com>
2025-09-02 11:58:57 +0530

    Update app.py

--- app.py
+++ app.py
@@ -97,7 +97,7 @@ class RelianceAutomation:
                 flow = Flow.from_client_config(
                     client_config=creds_data,
                     scopes=combined_scopes,
-                    redirect_uri="https://relianceautomation.streamlit.app/"  # Update with your actual URL
+                    redirect_uri="https://reliancegrn.streamlit.app/"  # Update with your actual URL
                 )
                 
                 # Generate authorization URL
